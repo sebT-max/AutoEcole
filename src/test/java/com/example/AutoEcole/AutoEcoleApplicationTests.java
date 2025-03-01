fc0409052040da52ea741b57c56bebc746ecfdf6
Sébastien Ternest <sebt-max@MacBook-Pro-de-Sebastien-Ternest.local>
2025-03-01 09:18:26 +0100

    Premier commit

---
+++ src/test/java/com/example/AutoEcole/AutoEcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutoEcole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
